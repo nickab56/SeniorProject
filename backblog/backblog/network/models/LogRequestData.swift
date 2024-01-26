41360ad9c69ec237dd8d3178372d30c106d100c8
Jake Buhite <jakebuhite@gmail.com>
2024-01-26 02:27:18 -0500

    Implement firestore service and started transferring repositories

--- backblog/backblog/network/models/LogRequestData.swift
+++ backblog/backblog/network/models/LogRequestData.swift
@@ -1,5 +1,5 @@
 //
-//  FriendRequestData.swift
+//  LogRequestData.swift
 //  backblog
 //
 //  Created by Jake Buhite on 1/21/24.
@@ -7,22 +7,23 @@
 
 import Foundation
 
-class FriendRequestData: Codable, Equatable {
-    static func == (lhs: FriendRequestData, rhs: FriendRequestData) -> Bool {
-        return lhs.targetId == rhs.targetId && lhs.senderId == rhs.senderId && lhs.requestDate == rhs.requestDate && lhs.isComplete == rhs.isComplete
+struct LogRequestData: Codable, Equatable {
+    static func == (lhs: LogRequestData, rhs: LogRequestData) -> Bool {
+        return lhs.targetId == rhs.targetId && lhs.senderId == rhs.senderId && lhs.requestDate == rhs.requestDate && lhs.isComplete == rhs.isComplete && lhs.logId == rhs.logId
     }
     
     var senderId: String?
     var targetId: String?
+    var logId: String?
     var requestDate: String?
     var isComplete: Bool?
     
     enum CodingKeys: String, CodingKey {
         case senderId = "sender_id"
         case targetId = "target_id"
+        case logId = "log_id"
         case requestDate = "request_date"
         case isComplete = "is_complete"
     }
+    
 }
-
-
---
+++ backblog/backblog/network/repository/MovieRepository.swift
@@ -0,0 +1,12 @@
+//
+//  MovieRepository.swift
+//  backblog
+//
+//  Created by Jake Buhite on 1/25/24.
+//
+
+import Foundation
+
+class MovieRepository {
+    
+}
