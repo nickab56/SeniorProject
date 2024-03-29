96cd6f20396618d7a84e5229407930d33150984d
Jake Buhite <jakebuhite@gmail.com>
2024-03-29 15:59:28 -0400

    Fixed movies not loading in real-time

--- backblog/backblog/LogViewModel.swift
+++ backblog/backblog/LogViewModel.swift
@@ -716,10 +716,7 @@ class LogViewModel: ObservableObject {
     }
     
     private func initLogListener() {
-        guard case .log(let fbLog) = log else {
-            self.fetchMovies()
-            return
-        }
+        guard case .log(let fbLog) = log else { return }
         guard let logId = fbLog.logId else { return }
         logListener = fb.getCollectionRef(refName: "logs")?.document(logId)
             .addSnapshotListener { documentSnapshot, error in
